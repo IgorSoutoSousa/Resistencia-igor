487d2e61ff0026e87f9caf5c7b9a663cb0ff11cb
IgorSoutoSousa <caboclocriacoes@gmail.com>
2021-05-01 00:30:09 -0300

    estagio 1

---
+++ Jedi.py
@@ -0,0 +1,6 @@
+class Jedi:
+    def __init__(self, nome, graduacao, especie, sabre):
+        self.nome = nome
+        self.graduacao = graduacao
+        self.especie = especie
+        self.sabre = sabre
